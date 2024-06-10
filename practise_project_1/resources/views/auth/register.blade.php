baeb56313b2f7764173e106bf464e1188ed594f8
JENIPHER <jeniperkuki@gmail.com>
2024-06-10 17:00:37 +0300

    modifying migrations

--- practise_project_1/resources/views/auth/register.blade.php
+++ practise_project_1/resources/views/auth/register.blade.php
@@ -42,7 +42,7 @@
                             <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="email" value="{{ old('username') }}"  autocomplete="username">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}"  autocomplete="username">
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
