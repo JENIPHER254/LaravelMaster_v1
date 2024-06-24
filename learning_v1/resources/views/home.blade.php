4d2c092e70ad0ce5c8cdaa75bccfd68d0ec694aa
JENIPHER <jeniperkuki@gmail.com>
2024-06-24 12:58:50 +0300

    google fonts configuration

--- learning_v1/resources/views/home.blade.php
+++ learning_v1/resources/views/home.blade.php
@@ -2,22 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+   <div class="row ">
+    <div class="col-3 d-flex justify-content-center align-items-center p-5">
+        <img src="images/freeCodeCamp.jpg" width="130" class="rounded-circle"alt="">
+    </div>
+    <div class="col-9 pt-5">
+        <div><h1>freeCodeCamp</h1></div>
+        <div class="d-flex">
+            <div class="pe-4"><strong>153 </strong>posts</div>
+            <div class="pe-4"><strong>23k </strong>followers</div>
+            <div class="pe-4"><strong>210 </strong>following</div>
         </div>
     </div>
+   </div>
 </div>
 @endsection
